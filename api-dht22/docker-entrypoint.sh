49047f0a016fa5c7f06017adcc186eb98d7e2156
Kauê França <kauefranca04@gmail.com>
2026-03-10 20:15:39 -0300

    fix: docker setup

--- api-dht22/docker-entrypoint.sh
+++ api-dht22/docker-entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "⏳ Waiting for PostgreSQL to be ready..."
-until npx prisma db execute --stdin <<< "SELECT 1" > /dev/null 2>&1; do
-  sleep 1
-done
-
 echo "🔄 Running Prisma migrations..."
 npx prisma migrate deploy
 
